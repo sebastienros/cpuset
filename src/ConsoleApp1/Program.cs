2c2bf541e3f39d18464ff0c30630dd95d93a768c
Sebastien Ros <sebastienros@gmail.com>
2024-01-10 16:27:23 -0800

    Create project

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Vanara.PInvoke;
+using static Vanara.PInvoke.Kernel32;
+using Process = System.Diagnostics.Process;
+
+uint[] cpuSet = [1,2,3];
+
+var process = new Process();
+
+// The executable should be in the same folder as the agent since it references the Console project
+// Use 'dotnet exec .dll' to use the current default dotnet version or the tests could fail if the .exe doesn't match what version is available locally
+process.StartInfo.FileName = "dotnet";
+process.StartInfo.Arguments = "exec" + " " + Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "ConsoleApp2.dll");
+
+// .NET doesn't respect a cpu affinity if a ratio is not set too. https://github.com/dotnet/runtime/issues/94364
+process.StartInfo.EnvironmentVariables.Add("DOTNET_PROCESSOR_COUNT", cpuSet.Length.ToString());
+process.StartInfo.EnvironmentVariables.Add("DOTNET_Thread_AssignCpuGroups", "0");
+process.StartInfo.EnvironmentVariables.Add("DOTNET_Thread_UseAllCpuGroups", "0");
+
+process.Start();
+Console.WriteLine($"[HOST] Process started: {process.Id}");
+
+var safeProcess = OpenProcess(ACCESS_MASK.MAXIMUM_ALLOWED, false, (uint)process.Id);
+var ssi = GetSystemCpuSetInformation(safeProcess).ToArray();
+var cpus = cpuSet.Select(i => ssi[i].CpuSet.Id).ToArray();
+var result = SetProcessDefaultCpuSets(safeProcess, cpus, (uint)cpus.Length);
+Console.WriteLine($"[HOST] Limiting CpuSet: {String.Join(',', cpuSet)} ({String.Join(',', cpus)}): {(result ? "SUCCESS" : "FAILED")}");
+Console.WriteLine("[HOST] Limits applied");
+
+process.WaitForExit();
+
+safeProcess.Dispose();
---
+++ src/ConsoleApp2/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Security.Cryptography;
+using Thread = System.Threading.Thread;
+
+
+var sw = Stopwatch.StartNew();
+
+// Wait for the host process to apply the JobObject before starting new threads.
+await Task.Delay(1000);
+
+Console.WriteLine("ConsoleApp2 started");
+Console.WriteLine($"Environment.ProcessorCount: {Environment.ProcessorCount}");
+
+// await RunOnThreadPoolAsync(200, 10000);
+RunOnThreads(200, 10000);
+
+Console.WriteLine("Terminated");
+
+void Work(CancellationToken cancellationToken)
+{
+    Console.WriteLine($"{sw.Elapsed} #: {Thread.GetCurrentProcessorId()} ThreadPool: {Thread.CurrentThread.IsThreadPoolThread}");
+    
+    while (!cancellationToken.IsCancellationRequested)
+    {
+        var i = RandomNumberGenerator.GetHexString(256);
+
+        if (i == "")
+        {
+            throw new Exception();
+        }
+    }
+}
+
+async Task RunOnThreadPoolAsync(int count, int milliseconds)
+{
+    var tasks = new List<Task>();
+
+    for (var i = 0; i < count; i++)
+    {
+        var t = Task.Factory.StartNew(() =>
+        {
+            var cancellationToken = new CancellationTokenSource(milliseconds).Token;
+            Work(cancellationToken);
+        });
+
+        await Task.Delay(500);
+        tasks.Add(t);
+    }
+
+    Task.WaitAll(tasks.ToArray());
+}
+
+void RunOnThreads(int count, int milliseconds)
+{
+    var threads = new List<Thread>();
+
+    for (var i = 0; i < count; i++)
+    {
+        var t = new Thread(() =>
+        {
+            var cancellationToken = new CancellationTokenSource(milliseconds).Token;
+            Work(cancellationToken);
+        });
+
+        t.Start();
+        Thread.Sleep(500);
+        threads.Add(t);
+    }
+
+    foreach (var t in threads)
+    {
+        t.Join();
+    }
+}
